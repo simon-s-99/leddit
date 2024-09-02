c088990b6065fb507d4498de2456d2860a296973
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-09-02 19:14:10 +0200

    feat: Create comment model

---
+++ comments/Comments/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace User.Data
+{
+    public class ApplicationDbContext
+    {
+        
+    }
+}
---
+++ comments/Comments/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Comments.Models
+{
+    public class Comment
+    {
+        [Required]
+        public Guid Id { get; set; }
+        //[Required]
+        //public User Author { get; set; }
+        //[Required]
+        //public Post Post { get; set; }
+        //public Comment? ReplyTo { get; set; } // Optional, the comment that the origional comment is in reply to
+        [Required]
+        [MaxLength(500)]
+        [Editable(true)]
+        public string Body { get; set; }
+    }
+}
