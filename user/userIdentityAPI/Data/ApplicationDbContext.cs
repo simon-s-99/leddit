ede77589beeca19421e5c230a5c2e6e38c672e3c
Sam <zaaamuel_96@hotmail.com>
2024-09-03 14:37:05 +0200

    Added ApplicationUser.cs

--- user/userIdentityAPI/Data/ApplicationDbContext.cs
+++ user/userIdentityAPI/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using userIdentityAPI.Models;
 
 namespace userIdentityAPI.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
---
+++ user/userIdentityAPI/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace userIdentityAPI.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+        public int Karma {  get; set; }
+    }
+}
