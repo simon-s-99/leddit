27f24b6f20d10e7e148616ba13c33d1c91394e24
gabrielwedin <gabbe_wedin@hotmail.com>
2024-09-17 15:56:33 +0200

    updated messageservice

--- post/post/Services/MessageService.cs
+++ post/post/Services/MessageService.cs
@@ -1,6 +1,45 @@
-﻿namespace post.Services
+﻿using System.Text;
+using RabbitMQ.Client;
+using System.Text.Json;
+using post.Models;
+
+namespace post.Services
 {
-    public class MessageService
+    public class MessageService : IHostedService
     {
+        private IConnection? connection;
+        private IModel? channel;
+
+        public void Connect()
+        { 
+            var connectionFactory = new ConnectionFactory { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+            connection = connectionFactory.CreateConnection();
+            channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("add-post", ExchangeType.Fanout);
+            channel.ExchangeDeclare("update-post", ExchangeType.Fanout);
+        }
+
+        public void NotifyPostChanged(string exchange, Post post)
+        {
+            var postJson = JsonSerializer.Serialize(post);
+            var message = Encoding.UTF8.GetBytes(postJson);
+
+            channel.BasicPublish(exchange, string.Empty, null, message);
+        }
+
+        public Task StartAsync(CancellationToken token)
+        {
+            Connect();
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken token)
+        {
+            channel?.Close();
+            connection?.Close();
+
+            return Task.CompletedTask;
+        }
     }
 }
