9363ea1c10a3d495843ab759a5af13de57a7246b
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-09-17 15:13:10 +0200

    refactor: comments/commented out swagger related lines

--- search/Program.cs
+++ search/Program.cs
@@ -11,17 +11,17 @@ namespace Search
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddEndpointsApiExplorer(); // swagger related? Remove?
+            builder.Services.AddSwaggerGen(); // swagger related? Remove?
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            // if (app.Environment.IsDevelopment())
+            // {
+            //     app.UseSwagger();
+            //     app.UseSwaggerUI();
+            // }
 
             app.UseHttpsRedirection();
 
