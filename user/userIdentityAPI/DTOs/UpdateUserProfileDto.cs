02c4cb3cfb9f3d678dd33c24ca409ea60b483b77
Sam <zaaamuel_96@hotmail.com>
2024-09-23 16:38:51 +0200

    Added UpdateUserProfileDto

---
+++ user/userIdentityAPI/DTOs/UpdateUserProfileDto.cs
@@ -0,0 +1,10 @@
+﻿namespace userIdentityAPI.DTOs
+{
+    public class UpdateUserProfileDto
+    {
+        public string DisplayName { get; set; }
+        public string Bio { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
