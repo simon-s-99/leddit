4fe1f34a44b96204b4dbf21bb6bf45ee0b395631
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-09-24 16:39:49 +0200

    feat: add ElasticsearchConnection as singleton
    also injects the singleton in the SearchController where it will later be used

--- search/Controllers/SearchController.cs
+++ search/Controllers/SearchController.cs
@@ -1,3 +1,4 @@
+using Elastic.Clients.Elasticsearch;
 using Microsoft.AspNetCore.Mvc;
 using Search.Models;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute; // avoids ambigous reference error
@@ -8,17 +9,23 @@ namespace Search.Controllers
     [Route("api/[controller]")]
     public class SearchController : ControllerBase
     {
+        private readonly IElasticsearchClientSettings _elasticsearchClientSettings;
+
+        public SearchController(IElasticsearchClientSettings elasticsearchClientSettings)
+        {
+            _elasticsearchClientSettings = elasticsearchClientSettings;
+        }
+
         [HttpGet(Name = "search")]
         public IEnumerable<SearchResult> Get()
         {
+            var client = new ElasticsearchClient(_elasticsearchClientSettings);
+
+            // TODO - implement actual searching here, connection seems to be working 
+
             // return Enumerable.ToArray() or something similar 
             SearchResult[] searchResults = []; // <-- remove this later, temporary code 
             return searchResults; // <-- remove this later, temporary code
         }
-
-        //
-        // Implement search/querying the elasticsearch db
-        // info here: https://www.elastic.co/guide/en/elasticsearch/client/net-api/current/getting-started-net.html#_operations 
-        //
     }
 }
