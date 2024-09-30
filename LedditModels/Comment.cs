842c86edda6b10874009dc5390c8c43715e5c157
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-09-30 12:18:02 +0200

    refactor: comment datamodel moved to classLibrary
    this is to make searching easier and to centralize changes to datamodels used in the project
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>

---
+++ LedditModels/Comment.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LedditModels
+{
+    public class Comment
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        //[Required]
+        //public Guid AuthorId { get; set; }
+
+        //[Required]
+        //public Guid PostId { get; set; }
+
+        public Guid? ReplyTo { get; set; } // Optional, the id of the comment that the original comment is in reply to
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        [Editable(true)]
+        public string Body { get; set; } = string.Empty;
+    }
+}
