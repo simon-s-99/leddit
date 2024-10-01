13ba6ae222de11577c1b0e8fb8e4a459d240704d
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-10-01 21:06:16 +0200

    search - pseudocode comments

--- search/Controllers/SearchController.cs
+++ search/Controllers/SearchController.cs
@@ -17,11 +17,18 @@ namespace Search.Controllers
         }
 
         [HttpGet(Name = "search")]
-        public IEnumerable<SearchResult> Get()
+        public IEnumerable<SearchResult> Get([FromQuery] string searchTerm)
         {
+            // would love to dispose this but it's thread safe & does not 
+            // implement IDispose soooo???? no?
             var client = new ElasticsearchClient(_elasticsearchClientSettings);
 
             // TODO - implement actual searching here, connection seems to be working 
+
+            // search based on the searchTerm
+            // do either free-text search if possible or do 
+            // search for one datamodel, then the next, then the next
+            // & put this in one list sorted in descending order for "searchWeight" 
 
             var query = Request.QueryString;
             var result = new SearchResult();
