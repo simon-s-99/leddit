40c7e05fa337fe4d9fd1be4e9462cc0461d91a58
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-10-02 23:32:29 +0200

    feat: freetext-search WIP

--- search/Controllers/SearchController.cs
+++ search/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 using Elastic.Clients.Elasticsearch;
 using Microsoft.AspNetCore.Mvc;
 using Search.Models;
+using Search.Services;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute; // avoids ambigous reference error
 
 namespace Search.Controllers
@@ -9,9 +10,9 @@ namespace Search.Controllers
     [Route("api/[controller]")]
     public class SearchController : ControllerBase
     {
-        private readonly IElasticsearchClientSettings _elasticsearchClientSettings;
+        private readonly ElasticsearchClientSettings _elasticsearchClientSettings;
 
-        public SearchController(IElasticsearchClientSettings elasticsearchClientSettings)
+        public SearchController(ElasticsearchClientSettings elasticsearchClientSettings)
         {
             _elasticsearchClientSettings = elasticsearchClientSettings;
         }
@@ -19,24 +20,8 @@ namespace Search.Controllers
         [HttpGet(Name = "search")]
         public IEnumerable<SearchResult> Get([FromQuery] string searchTerm)
         {
-            // would love to dispose this but it's thread safe & does not 
-            // implement IDispose soooo???? no?
-            var client = new ElasticsearchClient(_elasticsearchClientSettings);
-
-            // TODO - implement actual searching here, connection seems to be working 
-
-            // search based on the searchTerm
-            // do either free-text search if possible or do 
-            // search for one datamodel, then the next, then the next
-            // & put this in one list sorted in descending order for "searchWeight" 
-
-            var query = Request.QueryString;
-            var result = new SearchResult();
-            result.Test = query.ToString();
-
-            List<SearchResult> searchResults = new();
-
-            searchResults.Add(result);
+            // runs search logic & returns searchresults 
+            IEnumerable<SearchResult> searchResults = SearchService.Search(_elasticsearchClientSettings, searchTerm);
 
             return searchResults;
         }
--- search/Services/SearchService.cs
+++ search/Services/SearchService.cs
@@ -1,6 +1,35 @@
-﻿namespace Search.Services
+﻿using Elastic.Clients.Elasticsearch;
+using LedditModels;
+using Search.Models;
+
+namespace Search.Services
 {
-    public class SearchService
+    public static class SearchService
     {
+
+        // create multiple overloads that perform the search with 
+        // the given datamodel & query 
+
+        // search based on the searchTerm
+        // do either free-text search if possible or do 
+        // search for one datamodel, then the next, then the next
+        // & put this in one list sorted in descending order for "searchWeight" 
+
+        public static IEnumerable<SearchResult> Search(
+            ElasticsearchClientSettings elasticsearchClientSettings,
+            string searchTerm)
+        {
+            // ElasticClient is thread-safe and does not implement IDispose
+            var client = new ElasticsearchClient(elasticsearchClientSettings);
+
+            var commentSearchResponse = client.SearchAsync<Comment>(s => s
+                .Index("") // index to query
+                .From(0)
+                .Size(2)
+                .Query(q => q
+                    .Term() // column to query & actual query/term 
+                )
+            );
+        }
     }
 }
