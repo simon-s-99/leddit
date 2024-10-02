31fefa84820dea4c7301fcc2f28fb514f72d096d
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-10-02 21:22:53 +0200

    feat: cleanup in search program.cs

--- search/Program.cs
+++ search/Program.cs
@@ -9,33 +9,21 @@ namespace Search
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             // add elastisearchclient connection settings as a singleton
             // this is recommended in elasticsearchs documentation 
             builder.Services.AddSingleton<IElasticsearchClientSettings, ElasticsearchClientSettings>(sp =>
             {
                 string elasticConnString = "http://127.0.0.1:9200";
                 string elasticUsername = "elastic";
-                string elasticPassword = "dev";
+                string elasticPassword = "dev"; // change this in an actual production environment 
                 var elasticSettings = new ElasticsearchClientSettings(new Uri(elasticConnString))
                     .Authentication(new BasicAuthentication(elasticUsername, elasticPassword));
                 return elasticSettings;
             });
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer(); // swagger related? Remove?
-            builder.Services.AddSwaggerGen(); // swagger related? Remove?
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            // if (app.Environment.IsDevelopment())
-            // {
-            //     app.UseSwagger();
-            //     app.UseSwaggerUI();
-            // }
 
             app.UseHttpsRedirection();
 
