88cd51a2b51ed78ed49965579935c94d86e9b827
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-10-09 12:02:46 +0200

    feat: Edit method to return bool instead

--- comments/Services/MessageService.cs
+++ comments/Services/MessageService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Text.Json;
 using Comments.DTOs;
 using Microsoft.IdentityModel.Tokens;
+using System.Threading.Channels;
 
 namespace Comments.Services
 {
@@ -105,13 +106,15 @@ namespace Comments.Services
             channel.BasicConsume(queue, true, consumer);
         }
 
-        public Object? GetObject(Guid id, string service)
+        public bool CheckObjectExists(Guid id, string service)
         {
             // Connect to service
             httpClient = new HttpClient { BaseAddress = new Uri("http://" + service + ":8080") };
 
+            // Declare new http request
             HttpRequestMessage? request = new HttpRequestMessage(HttpMethod.Get, "");
 
+            // Request URI depends on passed service
             if (service == "post-service")
             {
                 request.RequestUri = new Uri("api/posts?id=" + id);
@@ -124,99 +127,15 @@ namespace Comments.Services
             // Send request to post-service
             var response = httpClient.Send(request);
 
-            // If response is 404, return null
+            // If response is 404, return false
             if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
             {
-                return null;
-            }
-
-            // Otherwise read the response
-            using var reader = new StreamReader(response.Content.ReadAsStream());
-            var json = reader.ReadToEnd();
-
-            try
-            {
-                // Read the json and return the post object
-                var post = JsonSerializer.Deserialize<Post>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-                return post;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
+                return false;
             }
 
             httpClient.Dispose();
 
-            return null;
-        }
-        public Post? GetPost(Guid id)
-        {
-            // Connect to service
-            httpClient = new HttpClient { BaseAddress = new Uri("http://post-service:8080") };
-
-            // Send request to post-service
-            var request = new HttpRequestMessage(HttpMethod.Get, "api/posts?id=" + id);
-            var response = httpClient.Send(request);
-
-            // If response is 404, return null
-            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-            {
-                return null;
-            }
-
-            // Otherwise read the response
-            using var reader = new StreamReader(response.Content.ReadAsStream());
-            var json = reader.ReadToEnd();
-
-            try
-            {
-                // Read the json and return the post object
-                var post = JsonSerializer.Deserialize<Post>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-                return post;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-
-            httpClient.Dispose();
-
-            return null;
-        }
-
-        public ApplicationUser? GetUser(Guid id)
-        {
-            // Connect to service
-            httpClient = new HttpClient { BaseAddress = new Uri("http://useridentityapi-service:8080") };
-
-            // Send request to user-service
-            var request = new HttpRequestMessage(HttpMethod.Get, "api/user/userid/" + id);
-            var response = httpClient.Send(request);
-
-            // If response is 404, return null
-            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-            {
-                return null;
-            }
-
-            // Otherwise read the response
-            using var reader = new StreamReader(response.Content.ReadAsStream());
-            var json = reader.ReadToEnd();
-
-            try
-            {
-                // Read the json and return the post object
-                var user = JsonSerializer.Deserialize<ApplicationUser>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-                return user;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-
-            httpClient.Dispose();
-
-            return null;
+            return true;
         }
     }
 }
