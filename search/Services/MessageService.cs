592674108371a0bfc58923d210bebbe6e2fa3c4b
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-10-09 12:28:18 +0200

    feat: add base MessageService template
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>

---
+++ search/Services/MessageService.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Search.Services
+{
+    public class MessageService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
