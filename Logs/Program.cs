ea23e9cf1032c67c155b7b4eb10931a3c6083255
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-10-11 10:34:29 +0200

    reformat: Remove unnecessary dependencies in Program.cs
    Co-Authored-By: Simon Sörqvist <simon.t.sorqvist@gmail.com>

--- Logs/Program.cs
+++ Logs/Program.cs
@@ -11,27 +11,15 @@ namespace Logs
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllers();
-
             var connectionString = builder.Configuration.GetConnectionString("kubernetes") ?? throw new InvalidOperationException("Connection string not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(connectionString));
 
             builder.Services.AddScoped<LogsService>();
-            builder.Services.AddEndpointsApiExplorer();
-
             builder.Services.AddHostedService<MessageService>();
             builder.Services.AddSingleton(s => s.GetServices<IHostedService>().OfType<MessageService>().First());
 
-            builder.Services.AddHttpClient();
             var app = builder.Build();
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
             app.Run();
         }
     }
