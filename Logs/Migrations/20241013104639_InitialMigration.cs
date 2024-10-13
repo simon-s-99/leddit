5ce1cd49595ecd635ab2ca1a4b2f7314e08b32f3
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-10-13 13:07:22 +0200

    fix: log migration trying to upldate db multiple times

--- Logs/Migrations/20241013104639_InitialMigration.cs
+++ Logs/Migrations/20241013104639_InitialMigration.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Logs.Migrations
 {
     /// <inheritdoc />
-    public partial class Initial : Migration
+    public partial class InitialMigration : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
